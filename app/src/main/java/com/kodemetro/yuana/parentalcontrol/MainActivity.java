17635ffcdd9204821e5f20f0a81b5087e822f75b
andhikayuana <andhikayuana@gmail.com>
2016-04-23 12:49:52 +0700

    sesuaikan drawer

--- app/src/main/java/com/kodemetro/yuana/parentalcontrol/MainActivity.java
+++ app/src/main/java/com/kodemetro/yuana/parentalcontrol/MainActivity.java
@@ -2,9 +2,8 @@ package com.kodemetro.yuana.parentalcontrol;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private long backPressedTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +51,14 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            long t = System.currentTimeMillis();
+            if (t - backPressedTime > 2000) {
+                backPressedTime = t;
+                Toast.makeText(this, "Tekan Back sekali lagi untuk keluar",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                super.onBackPressed();
+            }
         }
     }
 
@@ -80,20 +90,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_daftar_aplikasi) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_set_timer) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_materi) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_cara_pakai) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
